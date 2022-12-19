bd43f8eac52a82f0e9145cb1e1548149d29fff89
JD Calvelli <jdcalvelli@gmail.com>
2022-12-18 19:03:29 -0500

    adding line renderers for cords

---
+++ Assets/Scripts/PatchCordView.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatchCordView : MonoBehaviour
+{
+
+    [SerializeField] private Vector3 cordStartPos;
+    
+    private LineRenderer _lr;
+    
+    private void Start()
+    {
+        _lr = gameObject.GetComponent<LineRenderer>();
+    }
+
+    private void Update()
+    {
+        _lr.SetPosition(0, cordStartPos);
+        _lr.SetPosition(1, gameObject.transform.position);
+    }
+}
